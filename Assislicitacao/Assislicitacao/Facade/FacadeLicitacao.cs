980f30c74c00c5ab4a288b84234964852ad52c89
jp1204GORDOX <jpgertto.fernadesz1204@gmail.com>
2024-10-19 16:36:46 -0300

    FacadeLicitacao para chamar os serviços de licitacao

---
+++ Assislicitacao/Assislicitacao/Facade/FacadeLicitacao.cs
@@ -0,0 +1,10 @@
+﻿using Assislicitacao.Facade.Interfaces;
+using Assislicitacao.Models;
+
+namespace Assislicitacao.Facade {
+    public class FacadeLicitacao : IFacadeGeneric {
+        public bool Salvar(EntidadeDominio entidade) {
+            throw new NotImplementedException();
+        }
+    }
+}

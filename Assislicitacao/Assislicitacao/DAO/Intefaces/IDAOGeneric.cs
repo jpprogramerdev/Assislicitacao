381425cf1ca8e26addd79fdc64b978821da046ff
jp1204GORDOX <jpgertto.fernadesz1204@gmail.com>
2024-10-19 16:48:50 -0300

    DAO para remover responsabilidade de dados da controller

---
+++ Assislicitacao/Assislicitacao/DAO/Intefaces/IDAOGeneric.cs
@@ -0,0 +1,10 @@
+﻿using Assislicitacao.Models;
+
+namespace Assislicitacao.DAO.Intefaces {
+    public interface IDAOGeneric {
+        public bool Insert(EntidadeDominio entidade);
+        public List<EntidadeDominio> Select();
+        public bool Delete(EntidadeDominio entidade);
+        public bool Update(EntidadeDominio entidade);
+    }
+}

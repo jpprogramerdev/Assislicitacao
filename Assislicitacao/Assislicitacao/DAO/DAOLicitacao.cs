6afdbb511dd583de55988d63d05c3ee7cfd7b64d
jp1204GORDOX <jpgertto.fernadesz1204@gmail.com>
2024-10-19 20:44:04 -0300

    DAOLiticao para manipulação de dados com contexto de licitacao

---
+++ Assislicitacao/Assislicitacao/DAO/DAOLicitacao.cs
@@ -0,0 +1,22 @@
+﻿using Assislicitacao.DAO.Intefaces;
+using Assislicitacao.Models;
+
+namespace Assislicitacao.DAO {
+    public class DAOLicitacao : IDAOGeneric {
+        public bool Delete(EntidadeDominio entidade) {
+            throw new NotImplementedException();
+        }
+
+        public bool Insert(EntidadeDominio entidade) {
+            throw new NotImplementedException();
+        }
+
+        public List<EntidadeDominio> Select() {
+            throw new NotImplementedException();
+        }
+
+        public bool Update(EntidadeDominio entidade) {
+            throw new NotImplementedException();
+        }
+    }
+}

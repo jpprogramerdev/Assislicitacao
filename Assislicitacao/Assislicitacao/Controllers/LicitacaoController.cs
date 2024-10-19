9490b0088ee3504acf52123d90520eeb040453d3
jp1204GORDOX <jpgertto.fernadesz1204@gmail.com>
2024-10-19 16:22:15 -0300

    Tele de view mockada

---
+++ Assislicitacao/Assislicitacao/Controllers/LicitacaoController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Assislicitacao.Controllers {
+    public class LicitacaoController : Controller {
+        public IActionResult Cadastrar() {
+            return View();
+        }
+    }
+}
--- Assislicitacao/Assislicitacao/Program.cs
+++ Assislicitacao/Assislicitacao/Program.cs
@@ -21,6 +21,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Licitacao}/{action=Cadastrar}/{id?}");
 
 app.Run();

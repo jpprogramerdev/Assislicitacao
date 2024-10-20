9940d36ed382110d7a31d65c9e8d7f5b6212ca3c
jp1204GORDOX <jpgertto.fernadesz1204@gmail.com>
2024-10-19 21:13:54 -0300

    Trocado o nome da interface

--- Assislicitacao/Assislicitacao/Facade/Interfaces/IFacadeDatabase.cs
+++ Assislicitacao/Assislicitacao/Facade/Interfaces/IFacadeDatabase.cs
@@ -1,7 +1,7 @@
 ﻿using System.Data.SqlClient;
 
 namespace Assislicitacao.Facade.Interfaces {
-    public interface IFacadeSQLServer {
+    public interface IFacadeDatabase {
         public SqlConnection AbrirConexao();
         public void FecharConexao(SqlConnection conn);
     }

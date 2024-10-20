ea91c1532f2295caf748a41209d55cdf7b823527
jp1204GORDOX <jpgertto.fernadesz1204@gmail.com>
2024-10-19 21:43:28 -0300

    Implementado seleção de todos os tipos

---
+++ Assislicitacao/Assislicitacao/DAO/DAOTipoLicitacao.cs
@@ -0,0 +1,49 @@
+﻿using Assislicitacao.DAO.Intefaces;
+using Assislicitacao.Facade;
+using Assislicitacao.Facade.Interfaces;
+using Assislicitacao.Models;
+using System.Data.SqlClient;
+
+namespace Assislicitacao.DAO {
+    public class DAOTipoLicitacao : IDAOGeneric {
+        public IFacadeDatabase database { get; set; } 
+        public bool Delete(EntidadeDominio entidade) {
+            throw new NotImplementedException();
+        }
+
+        public bool Insert(EntidadeDominio entidade) {
+            throw new NotImplementedException();
+        }
+
+        public List<EntidadeDominio> Select() {
+            string Select = "SELECT * FROM TIPOS_LICITACAO;";
+
+            database = new FacadeSQLServer();
+
+            List<EntidadeDominio> ListaTipos = new();
+
+            using (SqlConnection conn = database.AbrirConexao()) {
+                using (SqlCommand query = new(Select, conn)) {
+                    using (SqlDataReader reader = query.ExecuteReader()) {
+                        while (reader.Read()) {
+                            TipoLicitacao Tipo = new TipoLicitacao{
+                                Id = reader.GetInt32(reader.GetOrdinal("TPL_ID")),
+                                Tipo = reader.GetString(reader.GetOrdinal("TPL_TIPO")),
+                                Sigla = reader.GetString(reader.GetOrdinal("TPL_SIGLA"))
+                            };
+
+                            ListaTipos.Add(Tipo);
+                        }
+                    }
+                    database.FecharConexao(conn);
+                }
+
+                return ListaTipos;
+            }
+        }
+
+        public bool Update(EntidadeDominio entidade) {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Assislicitacao/Assislicitacao/Facade/FacadeTipoLicitacao.cs
@@ -0,0 +1,19 @@
+﻿using Assislicitacao.DAO;
+using Assislicitacao.DAO.Intefaces;
+using Assislicitacao.Facade.Interfaces;
+using Assislicitacao.Models;
+
+namespace Assislicitacao.Facade {
+    public class FacadeTipoLicitacao : IFacadeGeneric {
+        public IDAOGeneric DAO { get; set; }
+
+        public bool Salvar(EntidadeDominio entidade) {
+            throw new NotImplementedException();
+        }
+
+        public List<EntidadeDominio> SelecionarTodos() {
+            DAO = new DAOLicitacao();
+            return DAO.Select();
+        }
+    }
+}

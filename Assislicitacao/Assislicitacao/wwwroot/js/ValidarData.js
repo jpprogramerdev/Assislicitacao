ebc2a0ed040c29f88feaf9621fa50962f2ba57bb
jp1204GORDOX <jpgertto.fernadesz1204@gmail.com>
2024-10-26 08:07:26 -0300

    Implementado verificação de data

---
+++ Assislicitacao/Assislicitacao/wwwroot/js/ValidarData.js
@@ -0,0 +1,16 @@
+﻿const dataLicitacao = document.getElementById("data-licitacao");
+
+function validateDate() {
+    const hoje = new Date();
+    hoje.setHours(0, 0, 0, 0);
+
+    const dataSelecionada = new Date(dataLicitacao.value);
+
+    if (dataSelecionada < hoje) {
+        dataLicitacao.setCustomValidity("A data da licitação deve ser maior ou igual à data atual.");
+    } else {
+        dataLicitacao.setCustomValidity("");
+    }
+}
+
+dataLicitacao.addEventListener("blur", validateDate);

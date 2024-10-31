9367c7a3a4e0e978a731c4a0046fea080e8aa87e
jp1204GORDOX <jpgertto.fernadesz1204@gmail.com>
2024-10-30 21:13:40 -0300

    Script de insert empresas

---
+++ Assislicitacao/Assislicitacao/Controllers/EmpresasController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Assislicitacao.Controllers {
+    public class EmpresasController : Controller {
+        public IActionResult Index() {
+            return View();
+        }
+    }
+}

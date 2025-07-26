f0d811f2393df26e476c61af4d4a941bd53f0bd5
jp1204GORDOX <jpgertto.fernadesz1204@gmail.com>
2025-07-26 10:35:32 -0300

    feat: adicionado a verificação da senha para o cadastro do usuário

---
+++ Assislicitacao/wwwroot/js/confirmPassword.js
@@ -0,0 +1,9 @@
+﻿document.querySelector('form').addEventListener('submit', function (e) {
+    const senha = document.getElementById('senha-usuario').value;
+    const confirmarSenha = document.getElementById('confirm-senha-usuario').value;
+
+    if (senha !== confirmarSenha) {
+        e.preventDefault(); 
+        alert('As senhas não coincidem.');
+    }
+});
